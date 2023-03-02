fa98a76d0ace4c6f93dd308c58a41ef299e5cee7
alexpozdnyakof <alexpozdnyakof@gmail.com>
2023-03-02 16:55:16 +0700

    add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
