2c6eb1cce08a0a216d9b81b1477027795d1e87d5
alexpozdnyakof <alexpozdnyakof@gmail.com>
2023-03-09 20:43:36 +0700

    add dark theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,31 @@ import TextField from './ui/text-field';
 function App() {
   return (
     <div className="signInPage">
-      <div className="surface">
-        <div className="stretch">
-          <h1> Welcome back</h1>
-        </div>
+      <Stack space="medium">
+        <div className="surface">
+          <div className="stretch">
+            <h1> Welcome back</h1>
+          </div>
 
-        <div className="stretch">
-          <Stack space="medium">
-            <Stack space="small">
-              <TextField
-                tone="negative"
-                message="Error"
-                label="Phone number, username, or email"
-              />
-              <TextField label="Password" type="password" />
+          <div className="stretch">
+            <Stack space="medium">
+              <Stack space="small">
+                <TextField
+                  tone="negative"
+                  label="Phone number, username, or email"
+                />
+                <TextField label="Password" type="password" />
+              </Stack>
+              <Button>Log in</Button>
             </Stack>
-            <Button>Log in</Button>
-          </Stack>
+          </div>
         </div>
-      </div>
+        <div className="surface">
+          <div className="signup">
+            Dont have an account?&nbsp;<a href="https://localhost">Sign up</a>
+          </div>
+        </div>
+      </Stack>
     </div>
   );
 }
