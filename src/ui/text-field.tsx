272f3e9025ce7ae6da25dd1dd8206f91a01b474f
alexpozdnyakof <alexpozdnyakof@gmail.com>
2023-03-09 13:30:40 +0700

    add forwardRef to text-field

--- src/ui/text-field.tsx
+++ src/ui/text-field.tsx
@@ -1,27 +1,29 @@
 import clsx from 'clsx';
+import { forwardRef } from 'react';
 import FormField, { ReusableFormFieldProps } from './base-field';
 import styles from './text-field.module.css';
 type TextFieldProps = ReusableFormFieldProps;
 
-export default function TextField({
-  label,
-  message,
-  tone,
-  type = 'text',
-  ...inputProps
-}: TextFieldProps) {
-  return (
-    <FormField label={label} message={message} tone={tone}>
-      {(drilled) => (
-        <div
-          className={clsx([
-            styles['input-wrapper'],
-            tone === 'negative' ? styles['input-wrapper_negative'] : null,
-          ])}
-        >
-          <input type={type} {...inputProps} {...drilled} />
-        </div>
-      )}
-    </FormField>
-  );
-}
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  function TextField(
+    { label, message, tone, type = 'text', ...inputProps },
+    ref,
+  ) {
+    return (
+      <FormField label={label} message={message} tone={tone}>
+        {(drilled) => (
+          <div
+            className={clsx([
+              styles['input-wrapper'],
+              tone === 'negative' ? styles['input-wrapper_negative'] : null,
+            ])}
+          >
+            <input type={type} ref={ref} {...inputProps} {...drilled} />
+          </div>
+        )}
+      </FormField>
+    );
+  },
+);
+
+export default TextField;
