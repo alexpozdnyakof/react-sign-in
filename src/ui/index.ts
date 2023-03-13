cb9bae82104e58424ef821b4aa3ef8b8a9ca3a33
alexpozdnyakof <alexpozdnyakof@gmail.com>
2023-03-13 16:03:46 +0700

    feat: add spinner

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as Stack } from './stack';
 export { default as Button } from './button';
+export { default as Spinner } from './spinner';
 export { default as TextField } from './text-field';
 export { default as PasswordField } from './password-field';
